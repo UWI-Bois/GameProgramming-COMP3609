59834cc58fbf3f0752d3004f305698d733e744b1
Ajay S <ajay_sieunarine@hotmail.com>
2019-11-06 23:44:29 -0400

    koby

---
+++ pusher_branch.sh
@@ -0,0 +1,9 @@
+git add .
+echo 'Enter commit message:'
+read commitMessage
+git commit -m "$commitMessage"
+echo 'Enter branch name:'
+read branchName
+git push origin $branchName
+echo 'Done!'
+read
